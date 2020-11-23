de1d28c365632eddb27daa2e3035e64064861fb1
buna26 <riyadasr26@gmail.com>
2020-11-23 21:44:31 +0530

    changed

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Palindrome {
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 2345;
+        int ans=0;
+        int temp = num;
+        while(num!=0){
+            int digit = num%10;
+            ans = ans*10+digit;
+            num/=10;
+        }
+        System.out.println(ans);
+        if(temp==ans){
+            System.out.println("palindrome");
+        }
+        else{System.out.println("not palindrome");}
+    }
 }
