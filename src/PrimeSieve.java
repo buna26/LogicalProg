e86e00c9ed26139b119da57713bef18401529472
buna26 <riyadasr26@gmail.com>
2021-01-11 10:44:11 +0530

    Prime numbers up to range using Sieve of Eratosthenes.

---
+++ src/PrimeSieve.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class PrimeSieve {
+    public static void primeSieve(boolean[] arr, int range){
+        for(int i=4; i<=range; i+=2){
+            arr[i] = false;
+        }
+        for(int i=3; i<=range; i+=2){
+            if(arr[i]){
+                for( int j=i*i; j<=range; j+=i){
+                    arr[j] = false;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int range = 30;
+        boolean[] primes = new boolean[range+1];
+        Arrays.fill(primes,true);
+        primeSieve(primes,range);
+        for(int i=1; i<=range; i++){
+            if(primes[i]){
+                System.out.println(i);
+            }
+        }
+    }
+}
